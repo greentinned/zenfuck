6e4eba292f2ba076161ab2f6f23950c4710940ea
Sergey K <skorotkov@yandex-team.ru>
2020-11-21 22:17:39 +0300

    clean up

--- src/main.zig
+++ src/main.zig
@@ -4,41 +4,41 @@ const stdin = std.io.getStdIn().reader();
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() anyerror!void {
-    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
-    defer arena.deinit();
+    const buf_size = 30_000;
+    var buffer: [buf_size]u8 = undefined;
+    var fixed_alloc = std.heap.FixedBufferAllocator.init(&buffer);
 
-    const args = try std.process.argsAlloc(&arena.allocator);
-    var input = std.ArrayList(u8).init(&arena.allocator);
+    var input = std.ArrayList(u8).init(&fixed_alloc.allocator);
+    defer input.deinit();
 
-    // read file
+    // read file or stdin
+    const args = try std.process.argsAlloc(&fixed_alloc.allocator);
 
     if (args.len > 1) {
         const file = try std.fs.openFileAbsolute(args[1], .{ .read = true });
         defer file.close();
-
-        try file.reader().readAllArrayList(&input, 30_000);
+        try file.reader().readAllArrayList(&input, buf_size);
     } else {
-        _ = try stdin.readAllArrayList(&input, 30_000);
+        try stdin.readAllArrayList(&input, buf_size);
     }
 
     try stdout.print("program: \n\n{}\n", .{input.items});
 
     // run input
+    var tape = std.ArrayList(u8).init(&fixed_alloc.allocator);
+    defer tape.deinit();
 
-    var tape = std.ArrayList(u8).init(&arena.allocator);
     try tape.append(0);
-
     _ = try run(false, &tape, input.items);
 }
 
 var code_pos: u32 = 0;
 var tape_pos: u32 = 0;
 
-const RunError = error{ Invalid, InvalidOp };
+const RunError = error{Invalid};
 
 fn run(skip: bool, tape: *std.ArrayList(u8), code: []const u8) RunError!bool {
     while (tape_pos >= 0 and code_pos < code.len) : (code_pos += 1) {
-
         // grow tape
         if (tape_pos >= tape.items.len) {
             try tape.append(0) catch |_| error.Invalid;
@@ -57,11 +57,11 @@ fn run(skip: bool, tape: *std.ArrayList(u8), code: []const u8) RunError!bool {
             switch (code[code_pos]) {
                 '+' => {
                     // inc value on current tap_pos
-                    tape.items[tape_pos] = tape.items[tape_pos] + 1;
+                    tape.items[tape_pos] = tape.items[tape_pos] +% 1;
                 },
                 '-' => {
                     // dec value on current tap_pos
-                    tape.items[tape_pos] = tape.items[tape_pos] - 1;
+                    tape.items[tape_pos] = tape.items[tape_pos] -% 1;
                 },
                 '.' => {
                     // write to stdout
